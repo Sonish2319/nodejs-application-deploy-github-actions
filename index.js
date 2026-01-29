e3377178f126a87a917e7329a4c4d44bcc13dde7
Sonish2319 <83855936+Sonish2319@users.noreply.github.com>
2026-01-29 11:52:22 +0545

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.port || 3000;
+
+
+app.get('/', (req,res)=>{
+    return res.send('Hello from Node.js Express Server');
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
